7d695b55632679e13c8072d9a072068c15b3ce79
Youjia <zyj@MacBook-Air-32.local>
2026-05-09 13:52:28 +0200

    ajustement des scripts

--- 1_entrainement.py
+++ 1_entrainement.py
@@ -4,37 +4,15 @@
 import re
 import pandas as pd
 import scipy.sparse as sp
+from pretraitement import pretraitement
  
 from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 import nltk
 
-# 1. prétraitement : nettoyer les données
-"""
-Il est noté qu'il existe des <br /> étiquettes
-"""
-nltk.download('stopwords', quiet=True)
-from nltk.corpus import stopwords
-stop_words = set(stopwords.words('english'))
 
-def pretraitement(text):
-    # retirer les html étiquettes par la librairie BeautifulSoup
-    text = BeautifulSoup(text, "html.parser").get_text()
-    
-    # en miniscule
-    text = text.lower()
-    
-    # supprimer les ponctuations et les symboles spéciales
-    text = re.sub(r'[^a-z\s]', '', text)
-    
-    # enlever les stopwords
-    words = text.split()
-    words = [w for w in words if w not in stop_words]
-    
-    return ' '.join(words)
-
-# 2. chargement des corpus
+# 1. chargement des corpus
 train_df = pd.read_csv('./dataset/imdb_train.csv')
 val_df   = pd.read_csv('./dataset/imdb_validation.csv')
 test_df  = pd.read_csv('./dataset/imdb_test.csv')
@@ -42,6 +20,11 @@ test_df  = pd.read_csv('./dataset/imdb_test.csv')
 print(f"Train : {len(train_df)} | Validation : {len(val_df)} | Test : {len(test_df)}")
 
 print("Terminé en charge des données d'entrainement.")
+
+# 2. prétraitement : nettoyer les données
+"""
+Il est noté qu'il existe des <br /> étiquettes
+"""
 
 print("Prétraitement en cours...")
 X_train_texte = train_df['review'].apply(pretraitement)
